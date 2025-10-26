8e7efc67f2706257ba7e050a07baa52314f84726
abebaw <abebawshita04@gmail.com>
2025-10-26 04:18:06 -0700

    Refactor user schema and update field names
    Signed-off-by: abebaw <abebawshita04@gmail.com>

--- models/user.js
+++ models/user.js
@@ -1,24 +1,28 @@
- import express from 'express'
- import mongoose from'mongoose'
- const userSchema=new mongoose.Schema(
-    {
-        _id:{type:String, required:true, },
-     Firestname:{type:String,required:true,},
-      Lastname:{type:String,required:true,},
-     
-      email: {type: String,required: true,unique: true,
-        lowercase: true}, password: {type: String,required: true,},
-        imageUrl:{ type:String, required:true, },
-    enrolledCourse:[
-            {
-          type:mongoose.Schema.Types.ObjectId,
-          ref:'Course'
-            }
-        ],
+import mongoose from 'mongoose';
 
-    },{
-        timestamps:true
-    }
- );
- const User=mongoose.model('user',userSchema);
- export default User
+const userSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true }, // Combined name
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: { type: String },
+    imageUrl: { type: String },
+    enrolledCourse: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+export default User;
