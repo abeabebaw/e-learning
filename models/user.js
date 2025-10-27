a93c6012da4f58598a0a86f2b3918c7a0788ea75
abebaw <abebawshita04@gmail.com>
2025-10-27 02:46:43 -0700

    Refactor user schema and model in user.js
    Signed-off-by: abebaw <abebawshita04@gmail.com>

--- models/user.js
+++ models/user.js
@@ -1,28 +1,24 @@
-// models/user.js
-import mongoose from 'mongoose';
+ import express from 'express'
+ import mongoose from'mongoose'
+ const userSchema=new mongoose.Schema(
+    {
+        _id:{type:String, required:true, },
+     Firestname:{type:String,required:true,},
+      Lastname:{type:String,required:true,},
+     
+      email: {type: String,required: true,unique: true,
+        lowercase: true}, password: {type: String,required: true,},
+        imageUrl:{ type:String, required:true, },
+    enrolledCourse:[
+            {
+          type:mongoose.Schema.Types.ObjectId,
+          ref:'Course'
+            }
+        ],
 
-const userSchema = new mongoose.Schema(
-  {
-    _id: { type: String, required: true },
-    firstName: { type: String, default: '' },
-    lastName: { type: String, default: '' },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
-    password: { type: String },
-    imageUrl: { type: String },
-    enrolledCourse: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Course',
-      },
-    ],
-  },
-  { timestamps: true }
-);
-
-const Users = mongoose.model('User', userSchema);
-export default Users;
+    },{
+        timestamps:true
+    }
+ );
+ const User=mongoose.model('user',userSchema);
+ export default User
